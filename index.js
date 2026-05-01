729669ead92f0b4202c7ed81ec586a9b62f2d8a4
M-Fernanda-art <fernanda201298@gmail.com>
2026-05-01 13:30:27 -0500

    subir rama

---
+++ index.js
@@ -0,0 +1,15 @@
+// CONDICIONALES ANIDADOS
+
+// Es una condición dentro de otra condición. Para esto se usa "if". Se utiliza 
+// cuando primero es necesario comprobar una condición general y luego una especifica.
+
+
+// -----Comprobar si un estudiante aprobó y luego si obtuvo nota sobresaliente-----
+
+const nota = prompt("por favor ingresa la nota")
+
+
+
+
+
+// ------Validar si un usuario ingresó contraseña correcta y luego si tiene permisos de administrador-----
